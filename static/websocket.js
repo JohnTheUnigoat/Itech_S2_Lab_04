e6b0001f452ab5bae88de2f72b0d50e8a2be672b
Ivan Hudzynskyi <ivanhudzinskyi2000@gmail.com>
2020-05-10 01:34:28 +0300

    Add client side checks, username storing
    Signed-off-by: Ivan Hudzynskyi <ivanhudzinskyi2000@gmail.com>

--- static/websocket.js
+++ static/websocket.js
@@ -1,13 +1,27 @@
 var socket = io()
 
 var username = document.getElementById("username");
+username.value = window.sessionStorage.getItem('username');
+
 var message = document.getElementById("message-text");
 
 var button = document.getElementById("send-btn");
 
 button.addEventListener('click', () => {
-    socket.emit('message', {
-        message: message.value,
-        username: username.value
-    });
+    if (username.value == '')
+    {
+        alert('Please enter your username!');
+        return;
+    }
+
+    window.sessionStorage.setItem('username', username.value);
+
+    if (message.value != ''){
+        console.log('Sending a message...');
+        socket.emit('message', {
+            message: message.value,
+            username: username.value
+        });
+        message.value = '';
+    }
 });
