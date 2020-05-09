344fcf6b75ae49309edd38aed3d59e26ba33969c
Ivan Hudzynskyi <ivanhudzinskyi2000@gmail.com>
2020-05-09 21:54:13 +0300

    Install websocket to client and server, test connection
    Signed-off-by: Ivan Hudzynskyi <ivanhudzinskyi2000@gmail.com>

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 const express = require('express');
+const socket = require('socket.io')
 
 const app = express();
 
 const server = app.listen(80);
+
+const io = socket(server);
+
+app.use(express.static('static'));
+
+io.on('connection', socket =>{
+    console.log('User connected!');
+});
